0a542169b6021e28fb59ee403a59eafbe0835fe3
Ilia Baryshnikov <qwelias@gmail.com>
2021-01-15 23:47:20 +0500

    fix: force update sw

--- docs/js/sw-register.js
+++ docs/js/sw-register.js
@@ -1,6 +1,7 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js').then((reg) => {
         console.log('/sw.js registered')
+        reg.update()
     }).catch((error) => {
         console.warn('/sw.js failed', error)
     })
