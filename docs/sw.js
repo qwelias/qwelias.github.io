9006f438194e0c50a60318ec7fe1e1aa1e20d619
Ilia Baryshnikov <qwelias@gmail.com>
2021-01-16 23:52:55 +0500

    fix(sw): handle preload reject

--- docs/sw.js
+++ docs/sw.js
@@ -17,19 +17,18 @@ sw.addEventListener('install', (/** @type {ExtendableEvent} */event) =>
 
 sw.addEventListener('activate', (/** @type {ExtendableEvent} */event) => {
     event.waitUntil(Promise.all([
-        caches.keys().then((names) => {
-            return Promise.all(names.map((name) => {
-                if (name === cacheName) return
-                return caches.delete(name)
-            }))
-        }),
+        caches.keys().then((names) => Promise.all(names.map((name) => {
+            if (name === cacheName) return
+            return caches.delete(name)
+        }))),
         sw.registration.navigationPreload ? self.registration.navigationPreload.enable() : null,
     ]))
 })
 
 self.addEventListener('fetch', (/** @type {FetchEvent} */event) =>
     event.respondWith(caches.open(cacheName).then(async cache => {
-        const res = await event.preloadResponse || await fetch(event.request).catch(() => null)
+        const preload = await Promise.resolve(event.preloadResponse).catch(() => null)
+        const res = preload || await fetch(event.request).catch(() => null)
         if (!res) return await cache.match(event.request) || cache.match('/offline.html')
 
         cache.put(event.request, res.clone())
